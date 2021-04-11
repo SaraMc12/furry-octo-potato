cea03123219f24e174fe3397a3fe2cc73ecf01ec
Sara McMullin <mcsara1221@gmail.com>
2021-04-10 20:51:43 -0400

    AppText Input component-now has icon

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import AppText from "./app/components/AppText";
-import { AntDesign } from "@expo/vector-icons";
-import Card from "./app/components/Card";
-import ListingDetailScreen from "./app/screens/ListingDetailScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import AccountScreen from "./app/screens/AccountScreen";
+import { AppOwnership } from "expo-constants";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Screen from "./app/components/Screen";
-import ListItem from "./app/components/ListItem";
-import Icon from "./app/components/Icon";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput placeholder="Name" icon="email" />
+    </Screen>
+  );
 }
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,10 +1,11 @@
 export default {
   primary: "#ee8285",
   secondary: "#00c2cb",
-  background: "#f6f3e4",
+  light: "#f8f4f4",
+  dark: "#0c0c0c",
   white: "#fff",
   black: "#000",
-  light: "#f8f4f4",
+
   accent: "#b0c6b9",
   delete: "tomato",
 };
