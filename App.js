7881174970064ea1174875bcfe05c9471920eded
Sara McMullin <mcsara1221@gmail.com>
2021-04-11 12:43:43 -0400

    refactoring Picker Component

--- App.js
+++ App.js
@@ -3,11 +3,19 @@ import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import Screen from "./app/components/Screen";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const catagories = [
+  { lable: "Cats", value: 1 },
+  { lable: "Doggos", value: 2 },
+  { lable: "Tiny Floof Critters", value: 3 },
+];
 
 export default function App() {
   return (
     <Screen>
-      <AppTextInput placeholder="Name" icon="email" />
+      <AppPicker items={catagories} icon="apps" placeholder="catagories" />
+      <AppTextInput placeholder="who farted" />
     </Screen>
   );
 }
